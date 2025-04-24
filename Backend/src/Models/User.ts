b38d59041aae1fb8b190a497b819d329afe3da4c
unknown <hannarong.bon@spumail.net>
2025-04-25 01:27:26 +0700

    Add lastName

--- Backend/src/Models/User.ts
+++ Backend/src/Models/User.ts
@@ -2,22 +2,20 @@
 import { Schema, model, Document } from 'mongoose';
 
 export interface IUser extends Document {
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
   role: 'customer' | 'admin';  // เพิ่ม role ไว้เผื่อมี admin
-  created_at: Date;
-  updated_at: Date;
 }
 
 const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
-}, { collection: 'Users' });
+}, { collection: 'Users' , timestamps: true});
 
 const User = model<IUser>('User', userSchema);
 export default User;
