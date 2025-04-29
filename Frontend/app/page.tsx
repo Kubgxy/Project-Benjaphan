18a781a16f74c93a4424c5033a3afc827c6d55bf
Kubgxy <hannarong.bon@spumail.net>
2025-04-30 04:49:25 +0700

    เพิ่มรูป bg frontend

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -63,10 +63,10 @@ export default function Home() {
       <Header />
 
       {/* Hero Section */}
-      <section className="relative h-[90vh]  flex items-center">
+      <section className="relative h-[100vh]  flex items-center">
         <div className="absolute inset-0 z-0">
           <Image
-            src="/production.jpg"
+            src="/bg/ChatGPT Image 30 เม.ย. 2568 04_39_21.png"
             alt="เครื่องประดับทองคำแท้"
             fill
             className="object-cover z-0"
